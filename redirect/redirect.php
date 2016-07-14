bb911ddf70c8b90a2e18b0600ae1f9b97d570ec7
Pierre Rudloff <contact@rudloff.pro>
2016-07-14 13:18:54 +0200

    WIP redirect

---
+++ redirect/redirect.php
@@ -0,0 +1,33 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+error_reporting(E_ALL^E_DEPRECATED);
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('{path:.*}', function (Request $request, Response $response) {
+    $params = $request->getQueryParams();
+    switch ($params['archiAffichage']) {
+        case 'adresseDetail':
+            $id = intval($params['archiIdAdresse']);
+            $a = new \archiAdresse();
+            $addressInfo = $a->getArrayAdresseFromIdAdresse($id);
+            $return = strip_tags(
+                $a->getIntituleAdresseFrom(
+                    $id,
+                    'idAdresse',
+                    array(
+                        'noHTML'=>true, 'noQuartier'=>true, 'noSousQuartier'=>true, 'noVille'=>true,
+                        'displayFirstTitreAdresse'=>true,
+                        'setSeparatorAfterTitle'=>'#'
+                    )
+                )
+            ).' ('.$addressInfo['nomVille'].')';
+            $return = explode('#', $return);
+            return $response->withRedirect('index.php/Adresse:'.$return[0]);
+            break;
+    }
+});
+$app->run();
