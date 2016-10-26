bb30a6b324883b8439875aae014a399a2ff1a5a8
Pierre Rudloff <contact@rudloff.pro>
2016-10-27 00:27:19 +0200

    Semantic properties on news too

--- LocalSettings.php
+++ LocalSettings.php
@@ -171,6 +171,7 @@ $wgNamespacesWithSubpages[NS_ADDRESS] = true;
 $wgVisualEditorAvailableNamespaces[NS_ADDRESS] = true;
 $wgVisualEditorAvailableNamespaces[NS_ADDRESS_NEWS] = true;
 $smwgNamespacesWithSemanticLinks[NS_ADDRESS] = true;
+$smwgNamespacesWithSemanticLinks[NS_ADDRESS_NEWS] = true;
 $smwgNamespacesWithSemanticLinks[NS_PERSON] = true;
 $wgNamespacesToBeSearchedDefault[NS_ADDRESS] = true;
 
