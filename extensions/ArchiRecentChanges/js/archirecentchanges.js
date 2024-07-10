70015ff6ab2ba066df4213550cdd262a7a8468a2
aphfug <aymericfeltz@gmail.com>
2024-07-10 15:21:26 +0200

    better image display and ordering (no setTimeout and account for slow connections) voir plus works but is very slow

--- extensions/ArchiRecentChanges/js/archirecentchanges.js
+++ extensions/ArchiRecentChanges/js/archirecentchanges.js
@@ -1,8 +1,12 @@
+var w;
+var height;
+var tmp;
+var width;
 var batch=0;
 function dispatchRecentChanges($new=0){
 	//fill the screen with columns of images
-	var w=window.innerWidth;
-	let height;
+	w=window.innerWidth;
+
 	//make it responsive
 	if(w==1920/4){
 		height=[100,100,100,100,100];
@@ -15,12 +19,9 @@ function dispatchRecentChanges($new=0){
 	} else {
 		height=[100];
 	}
-	var tmp=0;
-	var width=0;
+	tmp=0;
+	width=0;
 	$('.mw-special-ArchiRecentChanges .latest-changes-recent-change-container').each(function(){
-		
-		this.id=batch;
-		batch++;
 		$(this).css({
 			left: width + 'px',
 			top: height[tmp] + 'px',
@@ -38,30 +39,6 @@ function dispatchRecentChanges($new=0){
 	$(".mw-special-ArchiRecentChanges #content").css({
 		height: (Math.max.apply(null,height) +100)+ 'px'
 	});
-}
-function displayImages(){
-	$('.mw-special-ArchiRecentChanges .latest-changes-recent-change').each(function(){
-		
-        var $headerImage = $(this).find('a.image>img').first();
-		console.log($headerImage.parents('.thumb').is(':hidden'));
-		if (!($headerImage.parents('.thumb').is(':hidden'))) {
-        	var $headerImageUrl=$headerImage.attr('src');
-        	if(typeof($headerImageUrl)!='undefined'){
-        	    let $ImageUrl;
-        	    if($headerImageUrl.substr(0, $headerImageUrl.lastIndexOf('thumb') >= 0)){
-        	        $headerImageUrl=$headerImageUrl.substr(0, $headerImageUrl.lastIndexOf('/'));
-        	        $ImageUrl=$headerImageUrl.replace(/\/thumb/,'');
-        	    }
-        	    else{
-        	        $ImageUrl=$headerImageUrl;
-        	    }
-        	    var url = $(this).find('p > a').attr('href');
-        	    $(this).prepend('<a href="' + url + '"><img src="'+$ImageUrl+'" class="header-image" style="width:100%;height:100%;"></a>');
-        	    $headerImage.parents('.thumb').hide();
-
-        	}
-		}
-    });
 }
 
 async function getArbreCategories($title){
@@ -81,13 +58,14 @@ async function getArbreCategories($title){
 	for(var i in categorie){
 		tmp=await getArbreCategories("Catégorie:"+categorie[i]['title']);
 		if(tmp!="wrong"){
-			return tmp.push(categorie[i]['title']);
+			return tmp.concat([categorie[i]['title']]);
 		}
 	}
 	return "wrong";
 }
 
 async function addRecentChanges($rccontinue){
+	$("#voir-plus").hide();
 	var url = "https://www.archi-wiki.org/api.php";
 	var params ={
 		action: "query",
@@ -112,34 +90,8 @@ async function addRecentChanges($rccontinue){
 					var $recentChange = $('<article class="latest-changes-recent-change"></article>');
 					var $h3 = $('<h3><span id="'+recentchanges[rc].title+'" class="mw-headline"></span></h3>').text(recentchanges[rc].title.replace(/(Adresse:|Personne:)/g, '').replace(/\(.*\)/, '')); //remove the namespace and the city name
 					$recentChange.append($h3);
+
 					
-
-					/*var categoryTree=[];
-					await api.getCategories(recentchanges[rc].title).done(async function(data){
-						for(var cat in data){
-							await api.getCategories("Catégorie:"+data[cat]['title']).done(async function(data2){
-								if(data2.length>0){
-
-									await api.getCategories("Catégorie:"+data2[0]['title']).done(async function(data3){
-										if(data3.length>0){
-											await api.getCategories("Catégorie:"+data3[0]['title']).done(async function(data4){
-												if(data4.length>0){
-													categoryTree.push(data[cat]['title']);
-													categoryTree.push(data2[0]['title']);
-													categoryTree.push(data3[0]['title']);
-													categoryTree.push(data4[0]['title']);
-													
-												}
-											});
-										}
-									});
-								}
-							});
-						}
-					});
-					console.log(categoryTree);
-					*/
-					console.log("145: "+await getArbreCategories(recentchanges[rc].title));
 					var prop=await api.get({action: "ask", query: "[["+recentchanges[rc].title+"]]|?Image principale|?Adresse complète", format: "json"})
 						.then(function(response) {
 							var results = response.query.results;
@@ -155,48 +107,149 @@ async function addRecentChanges($rccontinue){
 						var $image=prop["printouts"]["Image principale"][0]['fulltext'];
 					}
 
+					var catégories=await getArbreCategories(recentchanges[rc].title);
+					if(catégories!="wrong"){
+						var stringCatégories='<a href="/catégorie:'+catégories[0]+'" title="Catégorie:'+catégories[0]+'">'+catégories[0]+'</a>';
+						if(catégories[1]){
+							stringCatégories+=' > <a href="/catégorie:'+catégories[1]+'" title="Catégorie:'+catégories[1]+'">'+catégories[1]+'</a>';
+							/*if(catégories[2] && !catégories[2].startsWith("Autre")){
+								stringCatégories+=' > <a href="/catégorie:'+catégories[2]+'" title="Catégorie:'+catégories[2]+'">'+catégories[2]+'</a>';
+
+							}*/
+						}
+						$recentChange.append(stringCatégories)
+					}
+					;
 
 					await api.parse('[['+$image+'|thumb|left|100px]]').done(function(data){
 						$recentChange.append($(data).find('div').first().html());
 					});
 
-					
-					/*$.ajax({
-						url: "extensions/ArchiRecentChanges/addWikiTextAsContent.php",
-						type: "POST",
-						dataType: "json",
-						data: {title: '[['+$image+'|thumb|left|100px]]'},
-						success: function(data, textstatus){
-							console.log(data);
-							console.log(data['result']);
-							$recentChange.append(data['result']);
-						},
-						error: function(xhr, status, error){
-							//var err=JSON.parse();
-						}
-					});*/
+					var date=new Date(recentchanges[rc].timestamp);
 
+					date=$('<p></p>').append($('<i></i>').text(date.getDay()+'/'+date.getMonth()+'/'+date.getFullYear()));
+					$recentChange.append(date);
+
+					var title=new mw.Title(recentchanges[rc].title);
+					await api.parse(title,{section:1}).done(function(data){
+						var text = $('<p></p>');
+
+						$(data).find('p').each(function() {
+							text.append($(this).html());
+						});
+						$(text).find('sup').remove();
+						$(text).find('a').contents().unwrap(); //remove links
+						text=$(text).text();
+						
+						
+						if(text.length>120){
+							var trimmedText = text.substring(0, 120);
+							trimmedText = trimmedText.substring(0, Math.min(trimmedText.length, trimmedText.lastIndexOf(" "))) //dosen't cut a word
+
+							$recentChange.append(trimmedText+'...');
+						}
+						else {
+							$recentChange.append(text);
+						}
+					});
+
+					$recentChange.append($('<p></p>').append($('<a></a>').attr('href', '/'+recentchanges[rc].title).attr('title',recentchanges[rc].title).text(mw.message('readthis').text())));
+					$recentChangeContainer.hide();
 					$recentChangeContainer.append($recentChange);
 					$(".latest-block").append($recentChangeContainer);
+					displayImage($recentChangeContainer);
+					
 				}
 			}
+			$("#voir-plus").data('val', response.continue.rccontinue);
+			$("#voir-plus").show();
 		}).catch(function(error){console.log(error);});
-	dispatchRecentChanges();
+	
 	
 }
-$(document).ready(function(){
 
-    //Put the image in the right place in ArchiRecentChanges
-    //displayImages();
-    setTimeout(() => {
-        dispatchRecentChanges();
-    }, 500);
+function displayImage($elt){
+	var $headerImage = $elt.find('a.image>img').first();
+	
+    	var $headerImageUrl=$headerImage.attr('src');
+    	if(typeof($headerImageUrl)!='undefined'){
+    	    let $ImageUrl;
+    	    if($headerImageUrl.substr(0, $headerImageUrl.lastIndexOf('thumb') >= 0)){
+    	        $headerImageUrl=$headerImageUrl.substr(0, $headerImageUrl.lastIndexOf('/'));
+    	        $ImageUrl=$headerImageUrl.replace(/\/thumb/,'');
+    	    }
+    	    else{
+    	        $ImageUrl=$headerImageUrl;
+    	    }
+    	    var url = $elt.find('p > a').attr('href');
+
+			var img=new Image();
+			img.onload=function(){orderOne($elt);};
+			img.src=$ImageUrl;
+			img.classList.add('header-image');
+			img.style.width='100%';
+			img.style.height='100%';
+			var $imageA=$('<a></a>').attr('href', url).append(img);
+    	    $elt.prepend($imageA);
+    	    $headerImage.parents('.thumb').hide();
+			$headerImage.parents('.thumbinner').hide();
+    	}
+	
+}
+function orderOne($elt){
+	$elt.css({
+		left: width + 'px',
+		top: height[tmp] + 'px',
+		width: height.length==1 ? '100%' : (100/height.length) + '%'
+	});
+	height[tmp]+=$elt.outerHeight();
+	width+=$elt.outerWidth();
+	tmp++;
+	if(tmp==height.length){
+		tmp=0;
+		width=0;
+	}
+	$(".mw-special-ArchiRecentChanges #content").css({
+		height: (Math.max.apply(null,height) +100)+ 'px'
+	});
+	$elt.show();
+}
+function orderAll(){
+	w=window.innerWidth;
+	//make it responsive
+	if(w==1920/4){
+		height=[100,100,100,100,100];
+	}else if(w>((1920/4)*3)){
+		height=[100,100,100,100];
+	} else if(w>((1920/4)*2)){
+		height=[100,100,100];
+	} else if(w>((1920/4)*1)){
+		height=[100,100];
+	} else {
+		height=[100];
+	}
+	tmp=0;
+	width=0;
+	$('.mw-special-ArchiRecentChanges .latest-changes-recent-change-container').each(function(){
+		displayImage($(this));
+		$(this).id=batch;
+		batch++;
+
+	});
+	
+}
+
+$(document).ready(function(){
+	$('.mw-special-ArchiRecentChanges .latest-changes-recent-change-container').each(function(){
+		$(this).hide();
+	});
+	orderAll();
+
     window.addEventListener('resize', function(){
         dispatchRecentChanges();
     },true);
-
+	
 	$("#voir-plus").click(function(){
 		addRecentChanges($(this).data('val'));
-		//displayImages();
 	});
 });
