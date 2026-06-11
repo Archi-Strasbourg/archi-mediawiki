0adeedbe23204668350c2e7eb84233d9d3b1c343
Yoan <ymartinez@insite.coop>
2026-06-11 12:22:20 +0200

    Remplissage auto date à afficher - #39428

--- extensions/ArchiTweaks/js/address-form.js
+++ extensions/ArchiTweaks/js/address-form.js
@@ -1,1 +1,41 @@
-// @todo Remplir le champ caché
+/*jshint esversion: 6 */
+/**
+ * Remplissage auto du champ "date1_afficher" selon les valeurs
+ * des champs "date1_debut" et "date1_fin".
+ */
+(() => {
+  'use strict';
+  const NAME_START = 'Infobox adresse[date1_début]';
+  const NAME_END = 'Infobox adresse[date1_fin]';
+  const NAME_RESULT = 'Infobox adresse[date1_afficher]';
+
+  const getInputs = () => {
+    return document.querySelectorAll(`input[name="${NAME_START}"], input[name="${NAME_END}"]`);
+  };
+
+  const generateString = () => {
+    const inputs = getInputs();
+    const values = [];
+    inputs.forEach(input => {
+      if (input.value !== '') {
+        values.push(input.value);
+      }
+    });
+    return values.join(' à ');
+  };
+
+  const onFieldChange = () => {
+    const resultInput = document.querySelector(`input[name="${NAME_RESULT}"]`);
+    if (resultInput) {
+      resultInput.value = generateString();
+    }
+  };
+
+  const initListeners = () => {
+    getInputs().forEach(input => {
+      input.addEventListener('change', onFieldChange);
+    });
+  };
+
+  initListeners();
+})();
